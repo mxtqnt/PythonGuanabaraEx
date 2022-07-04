f503f7d42af183582a32dbee58147bd8907506fd
Mxtqnt <anacarolinass.cc@gmail.com>
2022-07-04 20:18:09 -0300

    Initial

---
+++ 002.py
@@ -0,0 +1,2 @@
+nome = input('Me diga seu nome: ')
+print('Eae {}!'.format(nome))
---
+++ 003.py
@@ -0,0 +1,4 @@
+numero1 = int(input('1° Número: '))
+numero2 = int(input('2° Número: '))
+soma = numero1 + numero2
+print('A soma de {} e {} é {}!'.format(numero1, numero2, soma))
